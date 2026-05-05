c4cdcca60af5f34e473b2a34b596a38ce350379d
Thisaja Alwis <thisajaalwis@gmail.com>
2026-05-04 21:32:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Veto Backend
+Hello! This is the backend for the Veto app.
+
+We're using rust with the axum framework to handle http requests and postgresql to store data.
+To get this working you will need to first setup [rust](https://rust-lang.org/) and [postgres](https://www.postgresql.org/).
+
+To run:
+`cargo run`
+
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::{Router, routing::get};
+
+async fn hello() -> &'static str {
+    "thisajas"
+}
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/session/create", get(hello));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
